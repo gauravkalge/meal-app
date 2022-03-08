e4358583cb7c24926a0a417e775581eeb46da8c3
Gaurav Kalge <kalagegaurav@gmail.com>
2022-03-08 22:17:09 +0530

    169

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './categories_screen.dart';
-import 'category_meals.dart';
+import 'screens/categories_screen.dart';
+import 'screens/category_meals.dart';
 
 void main() => runApp(MyApp());
 
